e491931acdcda42b46dac6808a40a25a3226b2e2
Cem Mandraci <cemmandraci@hotmail.com>
2022-08-14 20:34:28 +0300

    #Created views and urls

--- ecommerce_con/store/views.py
+++ ecommerce_con/store/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+
+#Store view
+def store(request):
+    context = {}
+    return render(request, 'store/store.html', context)
+
+
+#Cart View
+def cart(request):
+    context = {}
+    return render(request, 'store/cart.html', context)
+
+
+#Checkout view
+def checkout(request):
+    context = {}
+    return render(request, 'store/checkout.html', context) 

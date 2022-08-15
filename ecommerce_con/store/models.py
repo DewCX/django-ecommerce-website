63ada677545377ebf8c48d892b7da1e08f0fa156
Cem Mandraci <cemmandraci@hotmail.com>
2022-08-15 21:47:56 +0300

    #Render images

--- ecommerce_con/store/models.py
+++ ecommerce_con/store/models.py
@@ -18,6 +18,7 @@ class Product(models.Model):
     price = models.FloatField()
     digital = models.BooleanField(default=False, null=True, blank=True)
     image = models.ImageField(null=True, blank =True)
+    
     def __str__(self):
         return self.name
 

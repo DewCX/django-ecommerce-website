8fc41ccc8553b3f41a8cabcf63dc81089a327ea9
Cem Mandraci <cemmandraci@hotmail.com>
2022-08-16 23:09:21 +0300

    #updateItem view logic

--- ecommerce_con/store/views.py
+++ ecommerce_con/store/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+import json
 
 from .models import *
 
@@ -51,4 +52,10 @@ def checkout(request):
 
 #Update Item
 def updateItem(request):
+    data = json.loads(request.data)
+    productId = data['productId']
+    action = data['action']
+
+    print('Action:', action)
+    print('productId:', productId)
     return JsonResponse('Item was added', safe=False)
